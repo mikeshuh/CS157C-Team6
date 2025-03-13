6cf89fd4763be056f9863538ccc58e6d4e855e0f
michaelvong <mvong006@ucr.edu>
2025-03-12 20:04:56 -0700

    added test code to test backend connection

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,16 @@
+'use client'
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/test")  // Ensure this matches your Flask route
+      .then((response) => response.json())
+      .then((data) => setData(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -96,6 +106,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <p>Backend Response: {data ? data : "Loading..."}</p>
     </div>
   );
 }
