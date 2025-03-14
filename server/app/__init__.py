e41b0957535050a5721af1add0651bd7bd72fc24
michaelvong <mvong006@ucr.edu>
2025-03-13 17:09:39 -0700

    setup mongodb connection

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 from app.database import mongo
 from app.routes import main  #blueprint
+from app.config import Config
 
 def create_app():
     app = Flask(__name__)
     
     # Load configuration
-    app.config.from_object("app.config.Config")
-
+    app.config.from_object(Config)
+    
     # Initialize database
     mongo.init_app(app)
 
@@ -17,5 +18,4 @@ def create_app():
 
     # Register Blueprints
     app.register_blueprint(main)
-
     return app
--- server/app/routes.py
+++ server/app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from flask import jsonify
 from app.database import mongo
 
 main = Blueprint("main", __name__)
@@ -6,7 +7,35 @@ main = Blueprint("main", __name__)
 def home():
     return "Hello, Flask!"
 
-
 @main.route('/api/test', methods=['GET'])
 def test():
+    """
+    Test if the server is running.
+    """
     return {"message": "Flask Backend is Running!"}
+@main.route('/api/test_db_connect', methods=['GET'])
+def test_connection():
+    """
+    Test connection to mongodb.
+    """
+    try:
+        mongo.db.command('ping')
+        return {"message": "Connected to MongoDB", 
+                "database": mongo.db.name}
+    except Exception as e:
+        return {"error": str(e)}
+
+@main.route('/api/test_db_insert', methods=['GET'])
+def test_insert():
+    """
+    Insert a test item into the test collection in briefly mongodb.
+    Returns:
+        dict: {'message': 'Inserted item into MongoDB'} or {'error': str(e)}
+    
+    """
+    try:
+        test_item = {"name" : "test item", "description" : "hello"}
+        mongo.db.test_collection.insert_one(test_item)
+        return {"message": "Inserted item into MongoDB"}
+    except Exception as e:
+        return {"error": str(e)}
