ebe0b9d031ce9320dd766f9618abd0bae2e65bb6
michaelvong <mvong006@ucr.edu>
2025-03-20 02:50:41 -0700

    connected to news api for articles

--- server/app/services/news_api.py
+++ server/app/services/news_api.py
@@ -1,26 +1,54 @@
 import os, json
 import requests #different from flask request
 from flask import jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class NewsApi:
     BASE_URL = "https://newsapi.org/v2/"
     def __init__(self):
-        self.api_key = os.getenv("NEWS_API_KEY")
+        self.api_key = os.environ.get("NEWS_API_KEY")
         
     def get_articles(self, params=None):
         '''
-        Fetch articles from the News API.'
+        Fetch articles from the News API.
+        Parameters: params 
         '''
         try:
             header = {"Authorization": self.api_key}
-            #response = requests.get(f"{self.BASE_URL}/everything?", params=params, headers=header)
+            response = requests.get(f"{self.BASE_URL}/everything?", params=params, headers=header)
 
             #file with dummy data to avoid repeated news_api calls
-            f = open('dummy.json', encoding="utf-8")
-            response = json.load(f)
-            print(params)
+            #f = open('dummy.json', encoding="utf-8")
+            #response = json.load(f)
+
+            #print(json.loads(response.text))
+            result_json = json.loads(response.text) #converts the json string to json
+            articles = result_json["articles"] #list of articles from news_api
+            processed_data = process_articles(articles)
+
             return jsonify({
-            "message": "testing"
+                "success": True,
+                "num_articles" : len(processed_data),
+                "processed_articles" : processed_data
             })
         except Exception as e:
             return {"error": str(e)}
+        
+def process_articles(articles):
+    '''
+    Takes the list of articles from News API, processes the data by getting the
+    url, title, author, published date. Returns a list of JSON objects that contain
+    these fields
+    '''
+    processed_articles = [] #list of json objs to return
+    for article in articles: 
+        p_article = {
+            "title": article["title"],
+            "author": article["author"],
+            "published_date": article["publishedAt"],
+            "url": article["url"]
+        }
+        processed_articles.append(p_article)
+    return processed_articles
