68f790d5fc235f9046980474c20123d9790ed50c
michaelvong <mvong006@ucr.edu>
2025-03-20 02:49:59 -0700

    added ai connection for summarization

---
+++ server/app/services/gemini.py
@@ -0,0 +1,32 @@
+import os, json
+from google import genai
+from dotenv import load_dotenv
+from flask import jsonify
+
+load_dotenv()
+
+client = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+
+class AI():
+    def __init__(self, client):
+        self.client = client
+        self.prompt = "You are a JSON only reponse generator. Always respond with valid JSON that matches the user's requested schema." +\
+        " Never include explanations or non-JSON text in your responses." +\
+        "Summarize this article into one paragraph and include a key points section with bullet points." +\
+        "In the key points section, only include the main points of the article. " +\
+        "Return as a JSON object. Here is the JSON schema: " +\
+        "Summary = {{'summary': str, 'key_points' : list[str]}}, " +\
+        "Here is the article: "
+
+    def summarize_article(self, content):
+        query = self.prompt + content
+        response = client.models.generate_content(
+            model="gemini-2.0-flash", contents=query
+        )
+        clean_json = response.text.replace("```json", "").replace("```", "").strip() #remove from gemini response
+        return jsonify({
+            "success": True,
+            "summarization" : json.loads(clean_json),
+        })
+
+ai_client = AI(client)
